2092d93bdf14141e631e83828b2e1c2c16914384
jroshlewis <47640194+jroshlewis@users.noreply.github.com>
2019-02-22 12:41:30 -0500

    Update LoginStepDefinition.rb

--- features/LoginStepDefinition.rb
+++ features/LoginStepDefinition.rb
@@ -6,7 +6,7 @@ Given /^The user is on the Home page on Facebook$/ do
 end
 
 When /^User enters invalid username and password$/ do
-      $driver.find_element(:id,'email').send_key("awqeet@mail.com")
+      $driver.find_element(:id,'email').send_key("awqe.com")
       $driver.find_element(:id,'pass').send_key("a123")
 end
 
@@ -16,9 +16,9 @@ end
 
 Then /^Error message should be displayed to the user$/ do
        wait = Selenium::WebDriver::Wait.new(:timeout => 30)
-       wait.until{$driver.find_element(:xpath,"//*[contains(text(),'The password you’ve entered is incorrect.')]")}
+       wait.until{$driver.find_element(:xpath,"//*[contains(text(),'The email address or phone number that you've entered doesn't match any account.')]")}
 
-      errMsg = $driver.find_element(:xpath,"//*[contains(text(),'The password you’ve entered is incorrect.')]").displayed?
+      errMsg = $driver.find_element(:xpath,"//*[contains(text(),'The email address or phone number that you've entered doesn't match any account.')]").displayed?
       if(errMsg.eql?true)
         puts"Error Message verified and Test Case Pass"
       else puts "Test Case Fail"
